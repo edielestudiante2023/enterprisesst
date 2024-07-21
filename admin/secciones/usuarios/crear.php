e847e9207d13e3d41a42da529747c555c9f0f146
Edi <146049944+edielestudiante2023@users.noreply.github.com>
2024-07-20 19:05:43 -0500

    20 julio

--- admin/secciones/usuarios/crear.php
+++ admin/secciones/usuarios/crear.php
@@ -2,8 +2,25 @@
 include("../../templates/header.php");
 ?>
 
+<div class="card">
+    <div class="card-header">Usuario</div>
+    <div class="card-body">
 
-crear usuarios
+        <form action="" method="post">
+
+            <div class="mb-3">
+                <label for="" class="form-label">Name</label>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+
+
+        </form>
+    </div>
+
+</div>
+
+
 
 
 <?php
