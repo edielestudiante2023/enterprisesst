ab901f63e615cbd4fd521cd807fa17b29bb18f25
edielestudiante2023 <edielestudiante2023@gmail.com>
2025-01-04 11:39:07 -0500

    Eliminado repositorio anidado en translations

---
+++ app/Models/ReporteModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ReporteModel extends Model
+{
+    protected $table = 'tbl_reporte';
+    protected $primaryKey = 'id_reporte';
+    protected $allowedFields = [
+        'titulo_reporte', 'id_detailreport', 'enlace', 'estado', 
+        'observaciones', 'id_cliente', 'created_at', 'updated_at', 'id_report_type'
+    ];
+    protected $useTimestamps = true;
+}
